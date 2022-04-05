6b91618d6122499a6948d0a6d6c6bede2d85f05d
Bradley Stouffer <voidmaelstrom@gmail.com>
2022-04-04 20:38:02 -0600

    Mocking HTTP Request for my Github page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 
 
 function App() {
+  const [gitHubName, setGitHubName] = useState('')     
+  const [gitHubURL, setGitHubURL] = useState('')   
+  // For Bonus
+  const [gitHubImageURL, setGitHubImageURL] = useState('')              
+
+
+  useEffect(() => {
+      fetch('https://api.github.com/users/voidmaelstrom')
+      .then(res => res.json())
+      .then(data => {
+        setGitHubName(data.name)
+        setGitHubURL(data.html_url)
+        setGitHubImageURL(data.avatar_url)
+      })
+  }, [])
+
   return (
     <div className="App">
-      <h1>Github Profile Info:</h1>
+      <div>
+        <h1>Github Profile Info:</h1>
+        <h2>{gitHubName}</h2>
+        <a href={gitHubURL}><button>Link to GitHub profile</button></a>
+      </div>
+      <div className="pt-5">
+        <img src={gitHubImageURL} alt='Github profile image' width='200' height='200'/>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default App
--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,14 @@
+import { render, screen, waitFor } from '@testing-library/react'
+import App from './App'
 
+beforeEach(() => {
+  // sets everything back to initial state before each test
+  fetch.resetMocks();
+})
 
-
+test("receives GitHub name from GitHub REST API using jest fetch mock", async () => {
+  fetch.mockResponseOnce(JSON.stringify({name: 'Bradley Stouffer'}))
+  render(<App />)
+  const gitHubName = await waitFor(() => screen.getByRole('heading', { level: 2 }))
+  expect(gitHubName).toHaveTextContent('Bradley Stouffer')
+})
